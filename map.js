d6aeac4c8601b0d33f19cc6e1eb253b5e57f1d36
Devdatta <devdatta@tengshe.in>
2016-06-10 09:51:37 +0530

    Moved JS code out of HTML file

---
+++ map.js
@@ -0,0 +1,84 @@
+	var shpfile, m ;
+    
+    function handleFileSelect(evt) {
+        var files = evt.target.files; // FileList object
+		var selFile = files[0];
+        handleFile(selFile);
+    }
+
+	
+	function handleFile(file){
+	var reader = new FileReader();
+        reader.onload = function(e) {
+            shpfile = new L.Shapefile(e.target.result,{isArrayBufer:true});
+			//console.log(shpfile);
+			shpfile.addTo(m);
+			shpfile.on("data:loaded", function (e){
+				m.fitBounds(shpfile.getBounds());
+			});
+        };
+
+        reader.onerror = function(e) {
+            console.log(e);
+        };
+        reader.readAsArrayBuffer(file);
+	
+	}
+
+    document.getElementById('file').addEventListener('change', handleFileSelect, false);
+	function init(){
+	
+		m = L.map('map').setView([40, -80], 4);
+		
+		var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+				maxZoom: 18,
+				attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+			});
+
+		OpenStreetMap_BlackAndWhite.addTo(m);
+		
+		m.on('layeradd', function (e){
+			//console.dir(e);
+		});
+
+		
+		//set up stuff
+		
+		var dropbox = document.getElementById("map");
+        dropbox.addEventListener("dragenter", dragenter, false);
+        dropbox.addEventListener("dragover", dragover, false);
+        dropbox.addEventListener("drop", drop, false);
+        dropbox.addEventListener("dragleave", function() {
+            m.scrollWheelZoom.enable();
+        }, false);
+
+        function dragenter(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            m.scrollWheelZoom.disable();
+        }
+
+        function dragover(e) {
+            e.stopPropagation();
+            e.preventDefault();
+        }
+
+        function drop(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            m.scrollWheelZoom.enable();
+            var dt = e.dataTransfer;
+            var files = dt.files;
+
+            var i = 0;
+            var len = files.length;
+            if (!len) {
+                return
+            }
+            while (i < len) {
+                handleFile(files[i]);
+                i++;
+            }
+        }
+	
+	}
